6e2aa9ca8fa0d419efbf49ec38ca43187109584d
Leon <leon390422914@gmail.com>
2020-07-03 19:00:50 +0800

    ci

--- .github/actions/build/index.js
+++ .github/actions/build/index.js
@@ -81,7 +81,7 @@ const runAction = () => {
 	verifyPackageJson();
 
 	// Copy "github_token" input variable to "GH_TOKEN" env variable (required by `electron-builder`)
-	setEnvVariable("GH_TOKEN", getEnvVariable("github_token", true));
+	// setEnvVariable("GH_TOKEN", getEnvVariable("github_token", true));
 
 	// Require code signing certificate and password if building for macOS. Export them to environment
 	// variables (required by `electron-builder`)
