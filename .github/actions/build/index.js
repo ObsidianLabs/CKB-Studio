5dd0b3fefea21583198320cb0e58c17858a97d29
Leon <leon390422914@gmail.com>
2020-07-03 23:29:03 +0800

    ci

--- .github/actions/build/index.js
+++ .github/actions/build/index.js
@@ -1,6 +1,6 @@
 const { chdir } = require('process');
 const { execSync } = require("child_process");
-const { existsSync, readFileSync, writeFileSync } = require("fs");
+const { existsSync, writeFileSync } = require("fs");
 
 const PACKAGE_JSON_PATH = "./package.json";
 
@@ -13,36 +13,39 @@ const log = msg => console.log(`\n${msg}`); // eslint-disable-line no-console
  * Exits the current process with an error code and message
  */
 const exit = msg => {
-	console.error(msg);
-	process.exit(1);
+  console.error(msg);
+  process.exit(1);
 };
 
 /**
  * Executes the provided shell command and redirects stdout/stderr to the console
  */
-const run = cmd => execSync(cmd, { encoding: "utf8", stdio: "inherit" });
+const run = cmd => execSync(cmd, {
+  encoding: "utf8",
+  stdio: "inherit"
+});
 
 /**
  * Exits if the `package.json` file is missing
  */
 const verifyPackageJson = () => {
-	if (!existsSync(PACKAGE_JSON_PATH)) {
-		exit("Missing `package.json` file");
-	}
+  if (!existsSync(PACKAGE_JSON_PATH)) {
+    exit("Missing `package.json` file");
+  }
 };
 
 /**
  * Determines the current operating system (one of ["mac", "windows", "linux"])
  */
 const getPlatform = () => {
-	switch (process.platform) {
-		case "darwin":
-			return "mac";
-		case "win32":
-			return "windows";
-		default:
-			return "linux";
-	}
+  switch (process.platform) {
+    case "darwin":
+      return "mac";
+    case "win32":
+      return "windows";
+    default:
+      return "linux";
+  }
 };
 
 /**
@@ -50,71 +53,61 @@ const getPlatform = () => {
  * program exits if the variable isn't defined
  */
 const getEnvVariable = (name, required = false) => {
-	const value = process.env[`INPUT_${name.toUpperCase()}`];
-	if (required && (value === undefined || value === null || value === "")) {
-		exit(`"${name}" input variable is not defined`);
-	}
-	return value;
+  const value = process.env[`INPUT_${name.toUpperCase()}`];
+  if (required && (value === undefined || value === null || value === "")) {
+    exit(`"${name}" input variable is not defined`);
+  }
+  return value;
 };
 
 /**
  * Sets the specified env variable if the value isn't empty
  */
 const setEnvVariable = (name, value) => {
-	if (value !== null && value !== undefined && value !== "") {
-		process.env[name] = value.toString();
-	}
+  if (value !== null && value !== undefined && value !== "") {
+    process.env[name] = value.toString();
+  }
 };
 
 /**
  * Installs NPM dependencies and builds/releases the Electron app
  */
 const runAction = () => {
-	const workingDirectory = getEnvVariable("working-directory")
-	if (workingDirectory) {
-		chdir(workingDirectory)
-	}
-	const platform = getPlatform();
-	const release = getEnvVariable("release") === "true";
+  const workingDirectory = getEnvVariable("working-directory")
+  if (workingDirectory) {
+    chdir(workingDirectory)
+  }
+  const platform = getPlatform();
 
-	// Make sure `package.json` file exists
-	verifyPackageJson();
+  // Make sure `package.json` file exists
+  verifyPackageJson();
 
-	// Copy "github_token" input variable to "GH_TOKEN" env variable (required by `electron-builder`)
-	// setEnvVariable("GH_TOKEN", getEnvVariable("github_token", true));
+  // Copy "github_token" input variable to "GH_TOKEN" env variable (required by `electron-builder`)
+  setEnvVariable("GH_TOKEN", getEnvVariable("github_token", true));
 
-	// Require code signing certificate and password if building for macOS. Export them to environment
-	// variables (required by `electron-builder`)
-	if (platform === "mac") {
-		setEnvVariable("CSC_LINK", getEnvVariable("mac_certs"));
-		setEnvVariable("CSC_KEY_PASSWORD", getEnvVariable("mac_certs_password"));
-	} else if (platform === "windows") {
-		setEnvVariable("CSC_LINK", getEnvVariable("windows_certs"));
-		setEnvVariable("CSC_KEY_PASSWORD", getEnvVariable("windows_certs_password"));
-	}
+  // Require code signing certificate and password if building for macOS. Export them to environment
+  // variables (required by `electron-builder`)
+  if (platform === "mac") {
+    setEnvVariable("CSC_LINK", getEnvVariable("mac_certs"));
+    setEnvVariable("CSC_KEY_PASSWORD", getEnvVariable("mac_certs_password"));
+  } else if (platform === "windows") {
+    setEnvVariable("CSC_LINK", getEnvVariable("windows_certs"));
+    setEnvVariable("CSC_KEY_PASSWORD", getEnvVariable("windows_certs_password"));
+  }
 
-	// Set Sentry token
-	// setEnvVariable("SENTRY_ORGANIZATION", getEnvVariable("sentry_organization"));
-	// setEnvVariable("SENTRY_PROJECT", getEnvVariable("sentry_project"));
-	// setEnvVariable("SENTRY_API_TOKEN", getEnvVariable("sentry_api_token"));
-	// setEnvVariable("UPLOAD_SOURCEMAPS", true);
+  setEnvVariable("BUILD", getEnvVariable("build"))
+  setEnvVariable("PROJECT_NAME", getEnvVariable("project_name"))
 
-	setEnvVariable("BUILD", getEnvVariable("build"))
-	setEnvVariable("PROJECT_NAME", getEnvVariable("project_name"))
+  writeFileSync('.npmrc', `@fortawesome:registry=https://npm.fontawesome.com/\n//npm.fontawesome.com/:_authToken=${getEnvVariable("fontawesome_token")}`)
 
-	writeFileSync('.npmrc', `@fortawesome:registry=https://npm.fontawesome.com/\n//npm.fontawesome.com/:_authToken=${getEnvVariable("fontawesome_token")}`)
+  // Disable console advertisements during install phase
+  setEnvVariable("ADBLOCK", true);
 
-	// Disable console advertisements during install phase
-	setEnvVariable("ADBLOCK", true);
+  const buildScript = getEnvVariable("build_script");
 
-	// Set CI=false
-	setEnvVariable("CI", false);
-
-	const buildScript = getEnvVariable("build_script");
-
-	if (buildScript) {
-		run(buildScript);
-	}
+  if (buildScript) {
+    run(buildScript);
+  }
 };
 
 runAction();
