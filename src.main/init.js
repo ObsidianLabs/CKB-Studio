65a892dc51868db2a3fe9df137692adc81267f2e
Leon <leon390422914@gmail.com>
2021-01-16 15:06:04 +0800

    feat: desktop login

--- src.main/init.js
+++ src.main/init.js
@@ -4,8 +4,9 @@ const { AutoUpdate } = require('@obsidians/global')
 const CompilerManager = require('@obsidians/ckb-compiler')
 const { CkbInstanceManager } = require('@obsidians/ckb-network')
 const ProjectChannel = require('@obsidians/ckb-project')
+const AuthChannel = require('@obsidians/auth')
 
-let ipcChannel, keypairManager, autoUpdate, compilerManager, instanceManager, projectChannel
+let ipcChannel, keypairManager, autoUpdate, compilerManager, instanceManager, projectChannel, authChannel
 module.exports = function () {
   ipcChannel = new IpcChannel()
   keypairManager = new KeypairManager(process.env.BUILD)
@@ -13,4 +14,5 @@ module.exports = function () {
   compilerManager = new CompilerManager()
   instanceManager = new CkbInstanceManager()
   projectChannel = new ProjectChannel()
+  authChannel = new AuthChannel()
 }
